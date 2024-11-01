c1bf88ddf5a0a1830a772166b40acfe718c4c051
THEPERFECTHUB2025 <samy.dumitruu16@gmail.com>
2024-11-01 14:42:17 +0100

    Update Window.lua

--- Window.lua
+++ Window.lua
@@ -1,17 +1,10 @@
--- Fonction pour créer une fenêtre Fluent
-local function createFluentWindow()
-    local Window = Fluent:CreateWindow({
-        Title = "https://discord.gg/KnP8KXaN " .. Fluent.Version,
-        SubTitle = "THE FUTURE LOCKER PAID \n V2 SOON \n Made By the_king.exploiter2025",
-        TabWidth = 160,
-        Size = UDim2.fromOffset(580, 460),
-        Acrylic = false,
-        Theme = "Darker",
-        MinimizeKey = Enum.KeyCode.Insert
-    })
-
-    return Window -- Retourne l'objet Window
-end
-
--- Appel de la fonction et stockage de la fenêtre créée
-local myWindow = createFluentWindow()
+-- Création de la fenêtre Fluent
+local Window = Fluent:CreateWindow({
+    Title = "https://discord.gg/KnP8KXaN " .. Fluent.Version,
+    SubTitle = "THE FUTURE LOCKER PAID \n V2 SOON \n Made By the_king.exploiter2025",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = false,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.Insert
+})
